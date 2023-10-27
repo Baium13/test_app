ca6380d143f798329a270c9b4aaa2f26089332bc
Fedorov Volodymyr <brodvovan@gmail.com>
2023-10-27 20:26:43 +0200

    Jason file with (username, password)

--- log.py
+++ log.py
@@ -1,29 +1,131 @@
 import json
+from datetime import datetime
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, make_response, redirect, session
 
 app = Flask(__name__)
+app.secret_key = '1'
+all_users = {}
+sessions = {}
 
-with open('login.jason') as f:
-    users = json.load(f)
+
+def read_users():
+    with open('users.json') as f:
+        users = json.load(f)
+
+        for user in users:
+            all_users.update({user['username']: user['password']})
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-
+    read_users()
+    response_type = "JSON"
+    if request.form:
+        data = request.form
+        response_type = "FORM"
+    else:
+        data = request.get_json()
+    response = None
     if not data or 'username' not in data or 'password' not in data:
-        return jsonify({'error': 'Login failed'}), 400
+        response = jsonify({'error': 'Login failed'}), 400
 
     username = data['username']
     password = data['password']
 
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            return jsonify({'message': 'Login successful OK'})
+    try:
+        if all_users[username] == password:
+            response = jsonify({'message': 'Login successful OK'}), 200
+            sessions.update({str(hash(username)): {"username": username, "login_date": datetime.now()}})
+    except KeyError:
+        pass
+    if response is None:
+        response = jsonify({'error': 'Login failed'}), 401
+    if response_type == "JSON":
+        resp = make_response(response[0], response[1])
+        if response[1] == 200:
+            resp.set_cookie("session", str(hash(username)), max_age=3600)
+        return resp
+    if response[1] == 200:
+        error_message = "Login succeeded with code 200"
+    else:
+        error_message = f"Login failed with code {response[1]}"
+    if response[1] == 200:
+        resp = redirect("/welcome")
+        resp.set_cookie("session", str(hash(username)), max_age=3600)
+        return resp
+    return make_response(render_template("login.html", error=error_message), response[1])
 
-    return jsonify({'error': 'Login failed'}), 401
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    date_of_birth = request.form['date_of_birth']
+    user_data = {
+        'username': username,
+        'password': password,
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'date_of_birth': date_of_birth
+    }
+
+    with open("users.json", "r") as file:
+        data = json.load(file)
+        data.append(user_data)
+    with open("users.json", "w") as file:
+        json.dump(data, file, indent=2)
+
+    sessions.update({str(hash(username)): {"username": username, "login_date": datetime.now()}})
+    resp = redirect("/welcome")
+    resp.set_cookie("session", str(hash(username)), max_age=3600)
+    return resp
+
+
+@app.route('/login', methods=['GET'])
+def get_login_form():
+    if request.cookies.get("session") in sessions:
+        return redirect("/welcome")
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET'])
+def get_register_form():
+    if request.cookies.get("session") in sessions:
+        return redirect("/welcome")
+    return render_template("registration_form.html")
+
+
+@app.route('/welcome', methods=['GET'])
+def welcome():
+    if request.cookies.get("session") not in sessions:
+        return redirect("/login")
+    return render_template("welcome.html", username=sessions[request.cookies["session"]]["username"])
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
     app.run()
+
+"""
+Implement registration form with following fields:
+username:
+password:
+email:
+first name:
+last name:
+date fo birth:
+
+After registration user must be logged in automatically and his credentials + data are stored in the file
+
+Implement logout button in welcome.html when you click it it will log you out.
+"""
